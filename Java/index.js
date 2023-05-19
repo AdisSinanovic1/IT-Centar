691bcf4cf71cb78c9c0d76dddb7012bee17f1c12
Adis_Sinanovic <adissinanovic4@gmail.com>
2023-05-19 18:11:00 +0200

    Java pocetak

---
+++ Java/index.js
@@ -0,0 +1,64 @@
+
+
+let num1 = 1;
+let num2 = 2;
+
+if ( num1 < num2) {
+    console.log (num1)
+    num1++;
+    console.log (num1)
+}
+
+console.log (num1);
+
+
+
+let br = Number;
+
+
+
+
+let imeprezime = 'Pera';
+let godine = 30;
+
+if(godine >= godine ) {
+    console.log ('punoletan');
+} else{
+    console.log ('Maloletan');
+}
+
+let broj = '1';
+
+let parse = Number;
+let parse2 = parseInt;
+
+if (parse2 % 3 !== 0) {
+    console.log ('broj je deljiv sa 3')
+} else {
+    console.log ('broj nije deljiv sa 3')
+}
+
+
+
+
+let ime = prompt ('unesite ime');
+let prezime = prompt ('unesite prezime');
+ 
+
+if(ime === 'Adis' && prezime === 'Sinanovic' ) {
+    console.log ('uneli ste tacno')
+}else if(ime !== 'Adis' && prezime === 'Sinanvovi') {
+    console.log ('Ime nije ispravno')
+}else if(ime === 'Adis' && prezime !== 'Sinanovic') {
+    console.log  ('Prezime nije ispravno')
+} else if(ime !== 'Adis' && prezime !== 'Sinanovic') {
+    console.log ('Uneli ste pogresne podatke')
+}
+
+
+
+let password = prompt ('Unesite sifru')
+
+
+
+
