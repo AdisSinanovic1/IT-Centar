e7016af6fa18bc36074aa67a215d08dd25bd37a6
Adis_Sinanovic <adissinanovic@gmail.com>
2023-10-16 18:14:39 +0200

    test

---
+++ src/components/Cas 8/Osmi.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+function App() {
+
+    const [data, setData] = useState ({})
+    const [currentProd, setCurrentPiod] = useState (1)
+
+    useEffect
+}
+
+
---
+++ src/components/SestiCas/SestiCas.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SestiCas() {
+
+}
+
+function SestiCas() {
+    return <div>SestiCas</div>
+}
+
+
+export default SestiCas;
+
