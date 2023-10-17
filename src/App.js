6af493b8c5d9d843ed7d1a1cf2591bc14e4d3f45
Adis_Sinanovic <adissinanovic@gmail.com>
2023-10-17 16:00:00 +0200

    Replace package.json and add pagination WIP

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import DrugiDomaci from './components/DrugiDomaci/DrugiDomaci'
+
+const App = () => {
+  return (
+    <DrugiDomaci />
+  )
+}
+
+export default App
---
+++ src/components/DrugiDomaci/DrugiDomaci.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import KarticaDomaci from "./KarticaDomaci";
+import "./style.css";
+
+function DrugiDomaci() {
+  const [data, setData] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [search, setSearch] = useState("");
+  const [singleCategory, setSingleCategory] = useState("");
+
+  const BASE_URL = "https://dummyjson.com/products";
+
+  const getData2 = () => {
+    axios
+      .get(`${BASE_URL}/search?q=${search}`)
+      .then((res) => setData(res.data.products));
+  };
+
+  const getCategories = () => {
+    axios
+      .get(`${BASE_URL}/categories`)
+      .then((data) => setCategories(data.data));
+  };
+
+  const getDataByCategory = (category, searchParam = "") => {
+    axios
+      .get(`${BASE_URL}/category/${category}?q=${searchParam}`)
+      // .get(`${BASE_URL}/category/${singleCategory}`)
+      .then((res) => setData(res.data.products));
+  };
+
+  useEffect(() => {
+    getData2();
+    getCategories();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="searchWrapper">
+        <input
+          placeholder="Search..."
+          className="searchInput"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+            getDataByCategory(singleCategory, e.target.value);
+          }}
+        />
+        <img
+          onClick={getData2}
+          src="search-svgrepo-com.svg"
+          height={30}
+          width={30}
+        />
+
+        <select
+          placeholder="Izaberi kategoriju"
+          style={{
+            width: "200px",
+          }}
+          className="searchInput"
+          onChange={(e) => {
+            setSingleCategory(e.target.value);
+            getDataByCategory(e.target.value);
+          }}
+        >
+          <option value="" disabled selected>
+            Select your option
+          </option>
+          {categories.map((categorie) => (
+            <option key={categorie} value={categorie}>
+              {categorie}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="cardWrapper">
+        {/* <KarticaDomaci />
+        <KarticaDomaci /> */}
+        {data.map((product) => (
+          //   <KarticaDomaci
+          //     title={product.title}
+          //     brand={product.brand}
+          //     category={product.category}
+          //     description={product.description}
+          //     key={product.id}
+          //   />
+          <KarticaDomaci product={product} key={product.id} />
+        ))}
+      </div>
+      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '16px'}}>
+        <div style={{width: '40px', height:'40px', borderRadius: '50%', backgroundColor: 'blue', textAlign: 'center', color: 'white'}}>1</div>
+      </div>
+    </div>
+  );
+}
+
+export default DrugiDomaci;
